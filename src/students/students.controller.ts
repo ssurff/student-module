d97b84cb1b34dce2a2a68be7eab43aa8f71659bb
ssu_rff <miquiabas.chastine@gmail.com>
2025-01-11 09:15:51 +0800

    read function was addes

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { student } from './students.entity';
 
@@ -9,5 +9,15 @@ export class StudentsController {
     @Post()
     async createStudent(@Body() data: Partial<student>): Promise<student> {
         return this.studentService.createStudent(data);
+    }
+
+    @Get()
+    async getStudent(): Promise<student[]> {
+        return this.studentService.getStudents();
+    }
+
+    @Get(':id')
+    async getStudentById(@Param('id') id: number): Promise<student> {
+        return this.studentService.getStudentById(id);
     }
 }
